7d45c6581871608b3986c0c5390c733c85d1ad70
Christopher Mavros <mavrosxristoforos@gmail.com>
2017-06-19 10:04:55 +0000

    Implemented Updater Included PHP Info on all files

--- popfeed_2.5_3.0/helper.php
+++ popfeed_2.5_3.0/helper.php
@@ -1,4 +1,14 @@
 <?php
+/*------------------------------------------------------------------------
+# popfeed - PopFeed
+# ------------------------------------------------------------------------
+# author    Christopher Mavros - Mavrosxristoforos.com
+# copyright Copyright (C) 2008 Mavrosxristoforos.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.mavrosxristoforos.com
+# Technical Support:  Forum - http://www.mavrosxristoforos.com/support/forum
+-------------------------------------------------------------------------*/
+
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
--- popfeed_2.5_3.0/tmpl/default.php
+++ popfeed_2.5_3.0/tmpl/default.php
@@ -1,9 +1,13 @@
 <?php
-/*
-PopFeed Plugin, developed by Christopher Mavros, Mavrosxristoforos.com
-@Copyright Copyright (C) Christopher Mavros, since 2008.
-@license GNU/GPL http://www.gnu.org/copyleft/gpl.html
-*/
+/*------------------------------------------------------------------------
+# popfeed - PopFeed
+# ------------------------------------------------------------------------
+# author    Christopher Mavros - Mavrosxristoforos.com
+# copyright Copyright (C) 2008 Mavrosxristoforos.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.mavrosxristoforos.com
+# Technical Support:  Forum - http://www.mavrosxristoforos.com/support/forum
+-------------------------------------------------------------------------*/
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
